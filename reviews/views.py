421880abe17c2c254817f4a5cded467ab43b59a0
wamah <pythondeveloper21@gmail.com>
2023-03-13 07:48:07 +0000

    converted function based views to class based views

--- reviews/views.py
+++ reviews/views.py
@@ -1,27 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-from .models import Review
+from django.views import View
 # Create your views here.
 
 
-def review(request):
-    if request.method == 'POST':   #here we check the method request is working with either GET/POST/PUT/DELETE
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = Review(user_name=form.cleaned_data['user_name'],
-                            review_text=form.cleaned_data['review_text'],
-                            rating=form.cleaned_data['rating'])
-            review.save()
-        #entered_username = request.POST['username'] saves data from the request method to a variable
-        #this username we passed it in the form as name
-        # if entered_username =="" and len(entered_username) >= 100:
-        #     return render(request, 'rev iews/review.htm', {'has_error': True})
-            print(form.cleaned_data)
-            return HttpResponseRedirect("/thank-you")
-    else:
+
+#  if form.is_valid():
+#             review = Review(user_name=form.cleaned_data['user_name'],
+#                             review_text=form.cleaned_data['review_text'],
+#                             rating=form.cleaned_data['rating'])
+ #review.save()
+
+
+class ReviewView(View):
+    def get(self, request, *args, **kwargs):
         form = ReviewForm()
-    return render(request, 'reviews/review.htm', {'form':form})
+        return render(request, 'reviews/review.htm', {'form':form})
+    
+    
+        
+    def post(self, request, *args, **kwargs):
+         form = ReviewForm(request.POST)
+         if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+         return render(request, 'reviews/review.htm', {'form':form})
+    
+
+# def review(request):
+#     if request.method == 'POST':   #here we check the method request is working with either GET/POST/PUT/DELETE
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+           
+#         #entered_username = request.POST['username'] saves data from the request method to a variable
+#         #this username we passed it in the form as name
+#         # if entered_username =="" and len(entered_username) >= 100:
+#         #     return render(request, 'rev iews/review.htm', {'has_error': True})
+#           #  print(form.cleaned_data)
+#             return HttpResponseRedirect("/thank-you")
+#     else:
+#         form = ReviewForm()
+#     return render(request, 'reviews/review.htm', {'form':form})
 
 
 
