d6df066317a2bd9cad14259252026ad23695ad5e
wamah <pythondeveloper21@gmail.com>
2023-03-13 07:00:31 +0000

    added models to collect data from the form

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from reviews.models import Review
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "rating")
+    list_filter = ("user_name", "rating")
+
+admin.site.register(Review, ReviewAdmin)
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-
+from .models import Review
 # Create your views here.
 
 
@@ -9,6 +9,10 @@ def review(request):
     if request.method == 'POST':   #here we check the method request is working with either GET/POST/PUT/DELETE
         form = ReviewForm(request.POST)
         if form.is_valid():
+            review = Review(user_name=form.cleaned_data['user_name'],
+                            review_text=form.cleaned_data['review_text'],
+                            rating=form.cleaned_data['rating'])
+            review.save()
         #entered_username = request.POST['username'] saves data from the request method to a variable
         #this username we passed it in the form as name
         # if entered_username =="" and len(entered_username) >= 100:
