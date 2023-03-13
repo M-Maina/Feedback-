ccb2096f16357671cb43da7853d1e5b1d5bef384
wamah <pythondeveloper21@gmail.com>
2023-03-13 09:33:42 +0000

    Added Listview and DetailView

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView #only return template with get_context_data
+from django.views.generic import ListView, DetailView
+from .models import Review
 # Create your views here.
 
 
@@ -46,5 +49,48 @@ class ReviewView(View):
 
 
 
-def thank_you(request):
-    return render(request,'reviews/thank_you.htm')
+# def thank_you(request):
+#     return render(request,'reviews/thank_you.htm')
+
+
+class ThankYouView(TemplateView):
+   template_name = 'reviews/thank_you.htm'
+   
+   
+   def get_context_data(self, **kwargs):   #This code get all the set context date that we can return in TempateView
+       context = super().get_context_data(**kwargs)
+       context["message"] = "This Works"
+       return context
+   
+    
+    
+    
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.htm"
+    model = Review
+    context_object_name = "reviews" #we setting this instead of object_list that django provides
+    
+    # def get_queryset(self): 
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=3) very good to retrieve the needed ratings
+    #     return data
+    
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     reviews = Review.objects.all()
+    #     context["reviews"] = reviews
+    #     return context
+    
+    
+class SingleReviewView(TemplateView):
+    template_name = 'reviews/single_review.htm'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"] #this is the id we passed in the urls
+        selected_review = get_object_or_404(Review, pk=review_id)
+        context["review"] = selected_review
+        return context
+    
+    
